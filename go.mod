737b6cd813bede728de0dd60051a8c69a10ac56a
rolandnsharp <rolandnsharp@gmail.com>
2020-05-16 15:58:12 +1000

    install gorm and confirm it working with sqlite

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-user-service
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.1.16
+require (
+	github.com/jinzhu/gorm v1.9.12
+	github.com/labstack/echo/v4 v4.1.16
+)
--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
 	"go-user-service/handler/user"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                   // find product with id 1
+	db.First(&product, "code = ?", "L1212") // find product with code l1212
+
+	fmt.Println(product)
+
 	// Echo instance
 	e := echo.New()
 

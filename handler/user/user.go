92adfbde074f29ba65bdaef8b49e7176de378c1b
rolandnsharp <rolandnsharp@gmail.com>
2020-05-17 13:25:37 +1000

    move database example code into handler

--- handler/user/user.go
+++ handler/user/user.go
@@ -1,13 +1,32 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
+	"go-user-service/database"
+
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
-	// "go-user-service/model"
 )
 
-// Handler
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func RegisterUser(c echo.Context) error {
+
+	db := database.DBConnection
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                   // find product with id 1
+	db.First(&product, "code = ?", "L1212") // find product with code l1212
+
+	fmt.Println(product)
 	return c.String(http.StatusOK, "Hello, World!")
 }
--- main.go
+++ main.go
@@ -14,12 +14,6 @@ import (
 	"go-user-service/handler/user"
 )
 
-type Product struct {
-	gorm.Model
-	Code  string
-	Price uint
-}
-
 func initDatabase() {
 	var err error
 	database.DBConnection, err = gorm.Open("sqlite3", "users.db")
@@ -29,44 +23,25 @@ func initDatabase() {
 	fmt.Println("Connection Opened to Database")
 
 	// // Migrate the schema
-	database.DBConnection.AutoMigrate(&Product{})
+	// database.DBConnection.AutoMigrate(&Product{})
+	// fmt.Println("Database migrated.")
+
 }
 
 func main() {
-	// db, err := gorm.Open("sqlite3", "test.db")
-	// if err != nil {
-	// 	panic("failed to connect database")
-	// }
-	// defer db.Close()
-
-	// // Migrate the schema
-	// db.AutoMigrate(&Product{})
-	// // Create
-	// db.Create(&Product{Code: "L1212", Price: 1000})
-
-	// // Read
-	// var product Product
-	// db.First(&product, 1)                   // find product with id 1
-	// db.First(&product, "code = ?", "L1212") // find product with code l1212
-
-	// fmt.Println(product)
 
 	initDatabase()
 
-	// Echo instance
 	e := echo.New()
 
-	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Routes
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "User Service.")
 	})
 	e.GET("/user", user.RegisterUser)
 
-	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 
 	defer database.DBConnection.Close()
